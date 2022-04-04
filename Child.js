9c6a4c407e67d5bf8cf435cd0301c8aef51aeb2a
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2022-04-04 15:22:48 +0300

    Update Child.js

--- Child.js
+++ Child.js
@@ -1,34 +1,27 @@
 import React from 'react';
 
 export class Child extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.handleChange = this.handleChange.bind(this)
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(e) {
     const name = e.target.value;
-    this.props.onChange(name); 
+    this.props.onChange(name);
   }
 
   render() {
     return (
       <div>
-        <h1>
-          Hey my name is {this.props.name}!
-        </h1>
-        <select onChange={this.handleChange} id="great-names">
-          <option value="Frarthur">
-            Frarthur
-          </option>
+        <select
+          id="great-names"
+          onChange={this.handleChange}>
 
-          <option value="Gromulus">
-            Gromulus
-          </option>
-
-          <option value="Thinkpiece">
-            Thinkpiece
-          </option>
+          <option value="Frarthur">Frarthur</option>
+          <option value="Gromulus">Gromulus</option>
+          <option value="Thinkpiece">Thinkpiece</option>
         </select>
       </div>
     );
