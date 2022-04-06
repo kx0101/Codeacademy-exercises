1495e6bd9fd410af4b84e4b54f8333768b0079f2
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2022-04-06 09:36:49 +0300

    Update Woofstagram.js

--- Woofstagram.js
+++ Woofstagram.js
@@ -1,90 +1,234 @@
-import React, {useState} from 'react';
-import { View, Text, TextInput, ScrollView } from 'react-native';
+import * as React from 'react';
+import { Image, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 
-function App() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [name, setName] = useState("");
-  const [birthday, setBirthday] = useState("");
-  const [breed, setBreed] = useState("");
-  const [toy, setToy] = useState("");
- 
-  function confirmPasswordsMatch(props) {
-    // check password against confirmation password
-    const { nativeEvent: { text } } = props;
+// Basic reusable components
 
-    if (text !== password) {
-      alert("Passwords do not match, please try again.")
-    }
-  }
+const Avatar = (props) => (
+  <Image
+    style={styles.avatar}
+    source={{ uri: props.url }}
+  />
+);
 
-return (
-  <ScrollView style={{ flex: 1, justifyContent: 'center', backgroundColor: '#ecf0f1' }}>
-    <InputWithLabel 
-    label="Email"
-    placeholder="Type your email here"
-    value={email}
-    onChangeText={setEmail}
-    />
-    <InputWithLabel 
-    label="Password"
-    placeholder="Type your password here"
-    value={password}
-    secureTextEntry
-    onChangeText={setPassword}
-    />
-    <InputWithLabel 
-    label="Confirm password"
-    placeholder="Confirm your password here"
-    value={confirmPassword}
-    secureTextEntry
-    onChangeText={setConfirmPassword}
-    onSubmitEditing={confirmPasswordsMatch}
-    />
-    <InputWithLabel 
-    label="Name"
-    placeholder="Type your pet's name here"
-    value={name}
-    onChangeText={setName}
-    />
-    <InputWithLabel 
-    label="Date of birth"
-    placeholder="Type your pet's birthday here"
-    value={birthday}
-    onChangeText={setBirthday}
-    />
-    <InputWithLabel 
-    label="Breed"
-    placeholder="Type your pet's breed here"
-    value={breed}
-    onChangeText={setBreed}
-    />
-    <InputWithLabel 
-    label="Toy"
-    placeholder="Type your pet's toy here"
-    value={toy}
-    onChangeText={setToy}
-    />
-  </ScrollView>
-  )
-}
+const Heading = (props) => (
+  <Text style={styles.heading}>
+    {props.children}
+  </Text>
+);
 
-function InputWithLabel(props) {
-  const {label, placeholder, value, onChangeText, secureTextEntry, onSubmitEditing} = props;
+const Title = (props) => (
+  <Text style={styles.title}>
+    {props.children}
+  </Text>
+);
 
-  return (
-    <View style={{ padding: 16 }}>
-      <Text style={{ padding: 8, fontSize: 18 }}>{label}</Text>
-      <TextInput placeholder={placeholder}
-      value={value}
-      onChangeText={onChangeText}
-      secureTextEntry={secureTextEntry}
-      onSubmitEditing={onSubmitEditing}
-      style={{ padding: 8, fontSize: 18}}
-       />
+const styles = StyleSheet.create({
+  avatar: {
+    width: 64,
+    height: 64,
+    borderRadius: "50%"
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: "600",
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingHorizontal: 24,
+    color: "#08060B"
+  },
+  title: {
+    fontSize: 12,
+    color: "#280D5F",
+    fontWeight: "600",
+    textTransform: "uppercase"
+  },
+});
+
+// App-specific components
+
+const WoofCard = (props) => (
+  <View style={woofCardStyles.card}>
+    <Avatar url={props.avatar} />
+    <View style={woofCardStyles.title}>
+      <Title>{props.name}</Title>
     </View>
-  );
-}
+  </View>
+);
+
+const woofCardStyles = StyleSheet.create({
+  card: {
+    width: 88,
+    height: 112,
+    padding: 12,
+    marginRight: 16,
+    borderWidth: 1,
+    borderColor: "#E7E3EB",
+    borderRadius: 12,
+    backgroundColor: "#FFFFFF"
+  },
+  title: {
+    textAlign: "center",
+    paddingTop: 8,
+  }
+});
+
+const WoofPost = (props) => (
+  <View style={woofPostStyles.layout}>
+    <Image source={{ uri: props.image }} style={woofPostStyles.image} />
+    <View style={woofPostStyles.content}>
+      <Title>{props.title}</Title>
+      <Text style={woofPostStyles.description} numberOfLines={2}>{props.description}</Text>
+    </View>
+  </View>
+);
+
+const woofPostStyles = StyleSheet.create({
+  layout: {
+    flexDirection: "row",
+    marginHorizontal: 24,
+    marginVertical: 8,
+  },
+  image: {
+    flex: 1,
+    borderRadius: 12,
+  },
+  content: {
+    flex: 2,
+    padding: 12
+  },
+  description: {
+    fontSize: 12,
+    marginTop: 4,
+    color: "#280D5f"
+  },
+});
+
+// The screen rendering everything
+const HomeScreen = () => (
+  <ScrollView>
+  <Heading>Trending Woofs</Heading>
+    <ScrollView horizontal>
+    {
+      data.woofs.map(woof => (
+        <WoofCard
+        key={woof.id}
+        name={woof.name}
+        avatar={woof.avatar}
+        />
+      ))
+    }
+    </ScrollView>
+
+    <Heading>New Woof posts</Heading>
+    {
+      data.posts.map(post => (
+        <WoofPost
+        key={post.id}
+        image={post.image}
+        title={post.title}
+        description={post.description}
+        />
+      ))
+    }
+
+  </ScrollView>
+);
+
+const App = () => (
+  <SafeAreaView style={{ flex: 1, backgroundColor: '#FAF9FA' }}>
+    <HomeScreen />
+  </SafeAreaView>
+);
 
 export default App;
+
+// "Fake" API data to use in your app
+const data = {
+  woofs: [
+    {
+      id: 'woof-1', 
+      name: 'Rex', 
+      avatar: 'https://images.unsplash.com/photo-1558788353-f76d92427f16?auto=format&fit=crop&w=648&q=80',
+      caretaker: 'Victor Grabarczyk',
+      source: 'https://unsplash.com/photos/x5oPmHmY3kQ',
+    },
+    {
+      id: 'woof-2', 
+      name: 'Ball', 
+      avatar: 'https://images.unsplash.com/photo-1585584114963-503344a119b0?auto=format&fit=crop&h=64&q=80',
+      caretaker: 'Tatiana Rodriguez',
+      source: 'https://unsplash.com/photos/J40C1k6Fut0',
+    },
+    {
+      id: 'woof-3', 
+      name: 'Happy', 
+      avatar: 'https://images.unsplash.com/photo-1543466835-00a7907e9de1?auto=format&fit=crop&h=64&q=80',
+      caretaker: 'Marliese Streefland',
+      source: 'https://unsplash.com/photos/2l0CWTpcChI',
+    },
+    {
+      id: 'woof-4',
+      name: 'Fluffy',
+      avatar: 'https://images.unsplash.com/photo-1554956615-1ba6dc39921b?auto=format&fit=crop&h=64&q=80',
+      caretaker: 'Nick Fewings',
+      source: 'https://unsplash.com/photos/rMKXLAIa2OY',
+    },
+    {
+      id: 'woof-5',
+      name: 'Spirit',
+      avatar: 'https://images.unsplash.com/photo-1514984879728-be0aff75a6e8?auto=format&fit=crop&h=64&q=80',
+      caretaker: 'Jamie Street',
+      source: 'https://unsplash.com/photos/uNNCs5kL70Q',
+    },
+  ],
+  posts: [
+    {
+      id: 'post-1',
+      image: 'https://images.unsplash.com/photo-1544568100-847a948585b9?auto=format&fit=crop&w=967&q=80',
+      title: 'Happy Woofs',
+      description: 'How to keep your woof health and happy. We\'ve asked some of the best experts out there.',
+      caretaker: 'Jamie Street',
+      source: 'https://unsplash.com/photos/UtrE5DcgEyg',
+    },
+    {
+      id: 'post-2',
+      image: 'https://images.unsplash.com/photo-1450778869180-41d0601e046e?auto=format&fit=crop&w=850&q=80',
+      title: 'Woofs & friends',
+      description: 'Best friends are important for humans, but also for dogs.',
+      caretaker: 'Krista Mangulsone',
+      source: 'https://unsplash.com/photos/9gz3wfHr65U',
+    },
+    {
+      id: 'post-3',
+      image: 'https://images.unsplash.com/photo-1558947530-cbcf6e9aeeae?auto=format&fit=crop&w=634&q=80',
+      title: 'Good Woofs',
+      description: 'A good woof is a woof that brings joy. Here are a few tips to let your woof behave.',
+      caretaker: 'Olia Nayda',
+      source: 'https://unsplash.com/photos/f6v_Q0WXEK8',
+    },
+    {
+      id: 'post-4',
+      image: 'https://images.unsplash.com/photo-1444212477490-ca407925329e?auto=format&fit=crop&w=1100&q=80',
+      title: 'Wild Woofs',
+      description: 'In some parts of the world, wild woofs are very common. Learn how to interact in a nice way.',
+      caretaker: 'Anoir Chafik',
+      source: 'https://unsplash.com/photos/2_3c4dIFYFU',
+    },
+    {
+      id: 'post-5',
+      image: 'https://images.unsplash.com/photo-1567014543648-e4391c989aab?auto=format&fit=crop&w=1050&q=80',
+      title: 'Sleepy Woofs',
+      description: 'Sleeping is just as important for woofs as it is for humans. What are the main things your woof dreams about.',
+      caretaker: 'Max Singh',
+      source: 'https://unsplash.com/photos/2637Pic9xMw',
+    },
+    {
+      id: 'post-6',
+      image: 'https://images.unsplash.com/photo-1524511751214-b0a384dd9afe?auto=format&fit=crop&w=967&q=80',
+      title: 'Exploring Woofs',
+      description: 'Just sitting in one place is boring for most woofs. How do woofs explore the world?',
+      caretaker: 'Jamie Street',
+      source: 'https://unsplash.com/photos/wcO2PWLuQ3U',
+    },
+  ],
+};
