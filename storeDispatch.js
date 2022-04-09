af8087e629fda703b592ef2280be3503ac94fba8
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2022-04-09 11:11:56 +0300

    Update storeDispatch.js

--- storeDispatch.js
+++ storeDispatch.js
@@ -1,61 +1,69 @@
-/* Note to learners: 
-Normally, you would import redux like this:
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 
-  import { createStore } from 'redux';
+// REDUX CODE
+///////////////////////////////////
 
-Due to Codecademy's technical limitations 
-for testing this exercise, we are using 
-`require()`.
-*/
-const { createStore } = require('redux');
-
-// Action Creators
-function increment() { 
-  return {type: 'increment'}
+const increment = () => {
+  return {type: 'increment'} 
 }
 
-function decrement() { 
+const decrement = () => { 
   return {type: 'decrement'}
 }
 
-// Reducer / Store
 const initialState = 0;
-const countReducer = (state = initialState, action) => {
+const counterReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'increment':
-      return state + 1; 
+      return state + 1;
     case 'decrement':
-      return state - 1; 
+      return state - 1;
     default:
-      return state;
+      return state; 
   }
-};  
-const store = createStore(countReducer);
+} 
 
-// HTML Elements
-const counterElement = document.getElementById('counter');
-const incrementer = document.getElementById('incrementer');
-const decrementer = document.getElementById('decrementer');
+const store = createStore(counterReducer);
 
-// Store State Change Listener
+// REACT CODE
+///////////////////////////////////
+
 const render = () => {
-  counterElement.innerHTML = store.getState();
-};
-store.subscribe(render)
+  ReactDOM.render(
+    <CounterApp 
+    state={store.getState()}
+    />,
+    document.getElementById('root')
+  )
+}
 render();
 
 
-// DOM Event Handlers
-const incrementerClicked = () => {
-  store.dispatch(increment())
-}
-incrementer.addEventListener('click', incrementerClicked);
+// Render once with the initial state.
+// Subscribe render to changes to the store's state.
+
+function CounterApp(props) {
+  const state = props.state;
+  
+  const onIncrementButtonClicked = () => {
+    // Dispatch an 'increment' action.
+    store.dispatch(increment());
+  }
  
-const decrementerClicked = () => {
-  store.dispatch(decrement());
+  const onDecrementButtonClicked = () => {
+    // Dispatch an 'decrement' action.
+    store.dispatch(decrement());
+  }
+  
+  return (   
+    <div id='counter-app'>
+      <h1> {state} </h1>
+      <button onClick={onIncrementButtonClicked}>+</button> 
+      <button onClick={onDecrementButtonClicked}>-</button>
+    </div>
+  )
 }
-decrementer.addEventListener('click', decrementerClicked);
 
-
-
-
+store.subscribe(render)
