ddc96542724795d724e28a9796ef3f33347bfdfb
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2022-04-11 10:45:37 +0300

    Create allRecipesSlice.js

---
+++ React-redux/allRecipesSlice.js
@@ -0,0 +1,37 @@
+// Selectors
+
+import allRecipesData from '../../../data.js'
+import { selectSearchTerm } from '../searchTerm/searchTermSlice.js';
+
+export const loadData = () => {
+  return {
+    type: 'allRecipes/loadData',
+    payload: allRecipesData
+  }
+}
+// Pre-loading the data so the application
+// outputs something without action dispatching
+const initialState = allRecipesData;
+export const allRecipesReducer = (allRecipes = initialState, action) => {
+  switch (action.type) {
+    case 'allRecipes/loadData':
+      return action.payload;
+    case 'favoriteRecipes/addRecipe':
+      return allRecipes.filter(recipe => recipe.id !== action.payload.id);
+    case 'favoriteRecipes/removeRecipe':
+      return [...allRecipes, action.payload]
+    default:
+      return allRecipes;
+  }
+}
+
+export const selectAllRecipes = (state) => state.allRecipes;
+
+export const selectFilteredAllRecipes = (state) => {
+  const allRecipes = selectAllRecipes(state);
+  const searchTerm = selectSearchTerm(state);
+
+  return allRecipes.filter((recipe) =>
+    recipe.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+};
