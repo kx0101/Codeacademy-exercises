890328d2eed141344b1b605f62a7e05992276b01
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2022-04-12 16:30:41 +0300

    Create createAsyncThunk.js

---
+++ middleware/createAsyncThunk.js
@@ -0,0 +1,27 @@
+import { fetchRecipes } from '../../app/api'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+
+const loadRecipes = createAsyncThunk('allRecipes/loadRecipes',
+  async (arg, thunkAPI) => {
+    const payload = await fetchRecipes();
+    const data = await payload.json();
+    return data;
+  }
+)
+
+export const allRecipesSlice = createSlice({
+  name: 'allRecipes',
+  initialState: {
+    recipes: [],
+    isLoading: false,
+    hasError: false,
+  },
+  reducers: {
+    addRecipes(state, action) {
+      state.recipes = action.payload
+    }
+  },  
+});
+
+export default allRecipesSlice.reducer;
