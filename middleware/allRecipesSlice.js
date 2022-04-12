36315efa1c8c6a08438a383993206430d2c1565e
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2022-04-12 16:43:55 +0300

    Create allRecipesSlice.js

---
+++ middleware/allRecipesSlice.js
@@ -0,0 +1,54 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import {
+  addFavoriteRecipe,
+  removeFavoriteRecipe,
+} from "../favoriteRecipes/favoriteRecipesSlice";
+import { selectSearchTerm } from "../search/searchSlice";
+export const loadRecipes = createAsyncThunk(
+  "allRecipes/getAllRecipes",
+  async () => {
+    const data = await fetch("api/recipes?limit=10");
+    const json = await data.json();
+    return json;
+  }
+);
+
+const sliceOptions = {
+  name: "allRecipes",
+  initialState: {
+    recipes: [],
+    isLoading: false,
+    hasError: false
+  },
+  reducers: {},
+  extraReducers: {
+    [loadRecipes.pending]: (state, action) => {
+      state.isLoading = true;
+      state.hasError = false;
+    },
+    [loadRecipes.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.hasError = false;
+      state.recipes = action.payload;
+    },
+    [loadRecipes.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.hasError = true;
+    }
+  }
+}
+
+export const allRecipesSlice = createSlice(sliceOptions);
+
+export const selectAllRecipes = (state) => state.allRecipes.recipes;
+
+export const selectFilteredAllRecipes = (state) => {
+  const allRecipes = selectAllRecipes(state);
+  const searchTerm = selectSearchTerm(state);
+
+  return allRecipes.filter((recipe) =>
+    recipe.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+};
+
+export default allRecipesSlice.reducer;
